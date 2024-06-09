27128d70117a3a366acaccfe36ed322ef429a84b
ozubrzycka <ozubrzycka@wp.pl>
2024-06-09 14:36:44 +0200

    Duża zmiana w zapisywaniu 5

--- wtyczkaoa/wtyczkaoa_dialog.py
+++ wtyczkaoa/wtyczkaoa_dialog.py
@@ -249,7 +249,7 @@ class wtyczkaoaDialog(QtWidgets.QDialog, FORM_CLASS):
                 if distance is not None:
                     file.write(f'Distance between points (point id:1- id:2) is: {distance:.3f} [m]\n')
     
-                azimuth, reverse_azimuth = self.calculate_and_display_azimuth()  # Update: Added azimuth calculation
+                azimuth, reverse_azimuth = self.azimuth_function()  # Update: Added azimuth calculation
                 if azimuth is not None and reverse_azimuth is not None:
                     file.write(f'Azimuth is (point id:1- id:2): {azimuth}\n')
                     file.write(f'Reverse azimuth is (point id:2- id:1): {reverse_azimuth}\n')  # Update: Added azimuth to file
